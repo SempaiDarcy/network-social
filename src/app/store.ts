b7e4157e58325714dd33cf669df41d1976fac1f0
chat <mr_darcy_1@mail.ru>
2024-03-08 16:56:38 +0600

    feat: Implement userSlice for managing user state, configure store with userSlice and api reducer, add auth middleware

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,21 +1,24 @@
-import type { Action, ThunkAction } from "@reduxjs/toolkit"
-import { configureStore } from "@reduxjs/toolkit"
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit"
 import { api } from "./services/api"
-
+import auth from "../features/user/userSlice"
+import { listenerMiddleware } from "../middleware/auth"
 
 export const store = configureStore({
   reducer: {
-    [api.reducerPath]: api.reducer
-  }
+    [api.reducerPath]: api.reducer,
+    auth,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware()
+      .concat(api.middleware)
+      .prepend(listenerMiddleware.middleware),
 })
 
-export type AppStore = typeof store
-
-export type AppDispatch = AppStore["dispatch"]
+export type AppDispatch = typeof store.dispatch
 export type RootState = ReturnType<typeof store.getState>
-export type AppThunk<ThunkReturnType = void> = ThunkAction<
-  ThunkReturnType,
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
   RootState,
   unknown,
-  Action
+  Action<string>
 >
